edb18691b7aa01fcb089b0fe2bdc5add71337757
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-21 09:13:27 -0500

    Created the NavBarLinks components and applied basic styling

--- src/components/globals/navbar/NavbarLinks.js
+++ src/components/globals/navbar/NavbarLinks.js
@@ -1,7 +1,49 @@
 import React, { Component } from "react"
+import styled from "styled-components"
+import { Link } from "gatsby"
+import { styles } from "../../../utils"
 
 export default class Navbar extends Component {
+  state = {
+    // The links are held in a state is to make adding/deleting any links much easier, without any hardcoding
+    links: [
+      {
+        id: 0,
+        path: "/",
+        name: "Home",
+      },
+      {
+        id: 1,
+        path: "/about/",
+        name: "About",
+      },
+      {
+        id: 2,
+        path: "/menu/",
+        name: "Menu",
+      },
+      {
+        id: 3,
+        path: "/contact/",
+        name: "Contact",
+      },
+    ],
+  }
   render() {
-    return <div>Test Content from Navbar Links</div>
+    return (
+      <LinkWrapper open={this.props.navbarOpen}>
+        {this.state.links.map(link => {
+          return (
+            <li key={link.id}>
+              <Link to={link.pah} className="nav-link">
+                {link.name}
+              </Link>
+            </li>
+          )
+        })}
+      </LinkWrapper>
+    )
   }
 }
+
+const LinkWrapper = styled.ul``
