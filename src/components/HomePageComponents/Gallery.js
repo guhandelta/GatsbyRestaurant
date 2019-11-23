91023692b75c94cf180986e6101a052aac4bbb1c
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-22 22:47:33 -0500

    Provided the necessary CSS styling for rendering the images and arranging them in a table, for different screen sizes

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -47,11 +47,11 @@ export default function Gallery() {
               </div>
               <div className="item item-2">
                 <Img fluid={img2} />
-                <p className="info">Awesome Pizza</p>
+                <p className="info">Awesome Pork</p>
               </div>
               <div className="item item-3">
                 <Img fluid={img3} />
-                <p className="info">Awesome Pizza</p>
+                <p className="info">Awesome Steak</p>
               </div>
             </GalleryWrapper>
           </Section>
@@ -61,4 +61,45 @@ export default function Gallery() {
   )
 }
 
-const GalleryWrapper = styled.div``
+const GalleryWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr; /*This attr says how many columns will be there and how the layout looks*/
+  grid-row-gap: 0.5rem;
+  grid-column-gap: 0.5rem;
+  .item {
+    position: relative;
+  }
+  .info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: ${styles.colors.mainGold};
+    padding: 0.1rem 0.3rem;
+    text-transform: capitalize;
+    border-radius: 0 0 7px 0;
+  }
+  @media (min-width: 576px) {
+    grid-template-columns: 1fr 1fr; /* fr-> fraction, this styl means, 2 columns of equal width*/
+    grid-column-gap: 1rem;
+  }
+  @media (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr); /* same as => 1fr 1fr 1fr ;*/
+  }
+  @media (min-width: 992px) {
+    .gatsby-image-wrapper {
+      height: 100%;
+    }
+    grid-template-areas:/* making a 4 col layout and pretty much describing how the gris will/shoul be displayed*/
+      "one one two two"
+      "one one three three";
+    .item-1 {
+      grid-area: one;
+    }
+    .item-2 {
+      grid-area: two;
+    }
+    .item-3 {
+      grid-area: three;
+    }
+  }
+`
