872e23b16fcab2d576f05b8f0172b2e105da2575
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-22 22:07:38 -0500

    Made requests to et multiple images to teh gallery

--- src/components/HomePageComponents/Gallery.js
+++ src/components/HomePageComponents/Gallery.js
@@ -13,24 +13,52 @@ const query = graphql`
         }
       }
     }
+    img2: file(relativePath: { eq: "homeGallery/img-2.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    img3: file(relativePath: { eq: "homeGallery/img-3.jpeg" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
   }
 `
 
 export default function Gallery() {
   return (
     <StaticQuery
-      query={graphql`
-        {
-          img1: file(relativePath: { eq: "homeGallery/img-1.jpeg" }) {
-            childImageSharp {
-              fluid(maxWidth: 500) {
-                ...GatsbyImageSharpFluid_tracedSVG
-              }
-            }
-          }
-        }
-      `}
-      render={data => <Img fluid={data.img1.childImageSharp.fluid} />}
+      query={query}
+      render={data => {
+        const img1 = data.img1.childImageSharp.fluid
+        const img2 = data.img2.childImageSharp.fluid
+        const img3 = data.img3.childImageSharp.fluid
+        return (
+          <Section>
+            <GalleryWrapper>
+              <div className="item item-1">
+                <Img fluid={img1} />
+                <p className="info">Awesome Pizza</p>
+              </div>
+              <div className="item item-2">
+                <Img fluid={img2} />
+                <p className="info">Awesome Pizza</p>
+              </div>
+              <div className="item item-3">
+                <Img fluid={img3} />
+                <p className="info">Awesome Pizza</p>
+              </div>
+            </GalleryWrapper>
+          </Section>
+        )
+      }}
     />
   )
 }
+
+const GalleryWrapper = styled.div``
