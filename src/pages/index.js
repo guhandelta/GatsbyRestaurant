b27abd124ec0cff2255221e7e6c03e03effb62d1
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2019-11-22 23:24:28 -0500

    Composed a single query to get all the images and modified the Gallery section code to display the images retrieved by the improvised GraphQL query

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { HomeHeader, Banner, BannerButton } from "../utils"
 import QuickInfo from "../components/HomePageComponents/QuickInfo"
-import Gallery from "../components/HomePageComponents/Gallery"
+import Gallery from "../components/HomePageComponents/Gallery-1"
 import img from "../images/bcg/homeBcg.jpeg"
 
 const IndexPage = () => (
